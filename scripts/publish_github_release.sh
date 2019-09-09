f3fa5bcebfafaf2ca04578e3c663e95efb2670e3
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2019-09-10 00:11:57 +0200

    build: publish to github and docker hub

---
+++ scripts/publish_github_release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+RELEASE_NOTES=`awk 'BEGIN {FS="##"; RS=""} FNR==3 {print; exit}' CHANGELOG.md`
+VERSION=`cat plugin.json|jq '.info.version'| sed s/\"//g`
+LATEST_TAG=$(git describe --tags --abbrev=0)
+
+if [ v${VERSION} == ${LATEST_TAG} ]; then
+  echo "Tag ${LATEST_TAG} have already been pushed. Exiting..."
+  exit 1
+fi
+
+git config user.email "eng@grafana.com"
+git config user.name "CircleCI Automation"
+git tag v${VERSION}
+git push origin v${VERSION}
+ghr \
+  -t ${GITHUB_TOKEN} \
+  -u ${CIRCLE_PROJECT_USERNAME} \
+  -r ${CIRCLE_PROJECT_REPONAME} \
+  -c ${CIRCLE_SHA1} \
+  -n "v${VERSION}" \
+  -b "${RELEASE_NOTES}" \
+  v${VERSION} \
+  ./artifacts/
