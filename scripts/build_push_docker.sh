f2109b8cd283efe844f3eb3cb61aff3b714f6720
bergquist <carl.bergquist@gmail.com>
2021-09-08 10:51:42 +0200

    push main to dockerhub
    Signed-off-by: bergquist <carl.bergquist@gmail.com>

--- scripts/build_push_docker.sh
+++ scripts/build_push_docker.sh
@@ -2,17 +2,20 @@
 
 set -e
 
-TAG=$(git describe --tags --abbrev=0 | cut -d "v" -f 2)
+TAG='v4.5.0'
+if [[ "$1" == "main" ]]; then
+  TAG="main-$(git rev-parse --short HEAD)"
+else
+  TAG=$(git describe --tags --abbrev=0 | cut -d "v" -f 2)
+fi
+
 echo "building ${TAG}"
 docker build -t ${IMAGE_NAME}:${TAG} .
-
-if [[ $TAG != *"beta"* ]]; then
-  docker tag ${IMAGE_NAME}:${TAG} ${IMAGE_NAME}:latest
-fi
 
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 docker push ${IMAGE_NAME}:${TAG}
 
-if [[ $TAG != *"beta"* ]]; then
+if [[ $TAG != *"beta"* ]] && [[ $TAG != *"main"* ]]; then
+  docker tag ${IMAGE_NAME}:${TAG} ${IMAGE_NAME}:latest
   docker push ${IMAGE_NAME}:latest
 fi
