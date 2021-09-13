4cd2f15bc348fa18ab7afea640e419c0a0e81320
bergquist <carl.bergquist@gmail.com>
2021-09-13 08:39:16 +0200

    rename main -> master
    Signed-off-by: bergquist <carl.bergquist@gmail.com>

--- scripts/build_push_docker.sh
+++ scripts/build_push_docker.sh
@@ -3,8 +3,8 @@
 set -e
 
 TAG=''
-if [[ "$1" == "main" ]]; then
-  TAG="main-$(git rev-parse --short HEAD)"
+if [[ "$1" == "master" ]]; then
+  TAG="master-$(git rev-parse --short HEAD)"
 else
   TAG=$(git describe --tags --abbrev=0 | cut -d "v" -f 2)
 fi
@@ -15,7 +15,7 @@ docker build -t ${IMAGE_NAME}:${TAG} .
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 docker push ${IMAGE_NAME}:${TAG}
 
-if [[ $TAG != *"beta"* ]] && [[ $TAG != *"main"* ]]; then
+if [[ $TAG != *"beta"* ]] && [[ $TAG != *"master"* ]]; then
   docker tag ${IMAGE_NAME}:${TAG} ${IMAGE_NAME}:latest
   docker push ${IMAGE_NAME}:latest
 fi
