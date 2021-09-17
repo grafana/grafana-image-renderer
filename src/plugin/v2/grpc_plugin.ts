57bdfb317d6b1265941fffc489e1f61751741b29
Agnès Toulet <35176601+AgnesToulet@users.noreply.github.com>
2021-09-17 08:52:24 +0200

    Metrics: update rafana_image_renderer_step_duration_seconds buckets (#287)

--- src/plugin/v2/grpc_plugin.ts
+++ src/plugin/v2/grpc_plugin.ts
@@ -279,7 +279,7 @@ const setupMetrics = (): PluginMetrics => {
       name: 'grafana_image_renderer_step_duration_seconds',
       help: 'duration histogram of browser steps for rendering an image labeled with: step',
       labelNames: ['step'],
-      buckets: [0.3, 0.5, 1, 2, 3, 5],
+      buckets: [0.1, 0.3, 0.5, 1, 3, 5, 10, 20, 30],
     }),
   };
 };
