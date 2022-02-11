f7d31ce876d6f66cb37f4c28e0d56c188f992ab4
Artur Wierzbicki <artur@arturwierzbicki.com>
2022-02-11 19:52:02 +0400

    #44449: use poolpeteer only for `contextPerRenderKey`

--- src/browser/clustered.ts
+++ src/browser/clustered.ts
@@ -1,9 +1,9 @@
-import { Cluster } from 'poolpeteer';
+import { Cluster as PoolpeteerCluster } from 'poolpeteer';
+import { Cluster as PuppeteerCluster } from 'puppeteer-cluster';
 import { ImageRenderOptions, RenderOptions } from '../types';
 import { Browser, RenderResponse, RenderCSVResponse, Metrics } from './browser';
 import { Logger } from '../logger';
 import { RenderingConfig, ClusteringConfig } from '../config';
-import { ConcurrencyImplementationClassType } from 'poolpeteer/dist/concurrency/ConcurrencyImplementation';
 
 enum RenderType {
   CSV = 'csv',
@@ -20,29 +20,36 @@ type ClusterResponse = RenderResponse | RenderCSVResponse;
 
 const contextPerRenderKey = 'contextPerRenderKey';
 
+type Cluster<JobData = any, ReturnData = any> = PuppeteerCluster<JobData, ReturnData> | PoolpeteerCluster<JobData, ReturnData>;
+
 export class ClusteredBrowser extends Browser {
   cluster: Cluster<ClusterOptions, ClusterResponse>;
   clusteringConfig: ClusteringConfig;
-  concurrency: number | ConcurrencyImplementationClassType;
+  concurrency: number;
 
   constructor(config: RenderingConfig, log: Logger, metrics: Metrics) {
     super(config, log, metrics);
 
     this.clusteringConfig = config.clustering;
-    this.concurrency = Cluster.CONCURRENCY_BROWSER;
+    this.concurrency = PuppeteerCluster.CONCURRENCY_BROWSER;
 
     if (this.clusteringConfig.mode === 'context') {
-      this.concurrency = Cluster.CONCURRENCY_CONTEXT;
+      this.concurrency = PuppeteerCluster.CONCURRENCY_CONTEXT;
     }
 
     if (this.clusteringConfig.mode === contextPerRenderKey) {
-      this.concurrency = Cluster.CONCURRENCY_CONTEXT_PER_REQUEST_GROUP;
+      this.concurrency = PoolpeteerCluster.CONCURRENCY_CONTEXT_PER_REQUEST_GROUP;
     }
   }
 
-  async start(): Promise<void> {
+  shouldUsePoolpeteer(): boolean {
+    return this.clusteringConfig.mode === contextPerRenderKey;
+  }
+
+  async createCluster(): Promise<Cluster<ClusterOptions, ClusterResponse>> {
     const launcherOptions = this.getLauncherOptions({});
-    this.cluster = await Cluster.launch<ClusterOptions, ClusterResponse>({
+
+    const clusterOptions = {
       concurrency: this.concurrency,
       workerShutdownTimeout: 5000,
       monitor: this.clusteringConfig.monitor,
@@ -50,7 +57,20 @@ export class ClusteredBrowser extends Browser {
       timeout: this.clusteringConfig.timeout * 1000,
       workerCreationDelay: 1000,
       puppeteerOptions: launcherOptions,
-    });
+    };
+
+    // TODO use poolpeteer by default after initial release and testing (8.5?)
+    if (this.shouldUsePoolpeteer()) {
+      this.log.debug('Launching Browser cluster with poolpeteer');
+      return PoolpeteerCluster.launch(clusterOptions);
+    }
+
+    this.log.debug('Launching Browser cluster with puppeteer-cluster');
+    return PuppeteerCluster.launch(clusterOptions);
+  }
+
+  async start(): Promise<void> {
+    this.cluster = await this.createCluster();
     await this.cluster.task(async ({ page, data }) => {
       if (data.options.timezone) {
         // set timezone
