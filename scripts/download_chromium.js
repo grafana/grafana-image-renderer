f3f9e52f556dfe0c5a36d2eaeb1813fd4bb99a7b
Clarity-89 <homes89@ukr.net>
2023-07-14 09:28:15 +0300

    Update chrome download

--- scripts/download_chromium.js
+++ scripts/download_chromium.js
@@ -1,48 +1,56 @@
 const path = require('path');
 const child_process = require('child_process');
 const Puppeteer = require('puppeteer');
-const { PUPPETEER_REVISIONS } = require('../node_modules/puppeteer/lib/cjs/puppeteer/revisions')
+const {BrowserPlatform, Browser, install, resolveBuildId, BrowserTag} = require('@puppeteer/browsers')
 
 const archArg = process.argv[2];
 let [
-  // Should be one of linux, mac, win32, win64 as per options in BrowserFetcher but we reuse the same arch string
-  // as for grpc download (ie darwin-x64-unknown) so we need to transform it a bit
-  platform,
-  arch
+    // Should be one of linux, mac, win32, win64 as per options in BrowserFetcher but we reuse the same arch string
+    // as for grpc download (ie darwin-x64-unknown) so we need to transform it a bit
+    platform,
+    arch
 ] = archArg.split('-');
 
 if (platform === 'win32' && arch === 'x64') {
-  platform = 'win64'
+    platform = BrowserPlatform.WIN64
 }
+
 
 if (platform === 'darwin') {
-  platform = 'mac'
+    platform = BrowserPlatform.MAC
 }
 
-const outputPath = "dist/" + (process.argv[3] || `plugin-${archArg}`);
+//const outputPath = "dist/" + (process.argv[3] || `plugin-${archArg}`);
+const outputPath = path.resolve(process.cwd(), "dist", process.argv[3] || `plugin-${archArg}`);
 
-const browserFetcher = Puppeteer.createBrowserFetcher({ platform });
-const revision = PUPPETEER_REVISIONS.chromium;
+// const browserFetcher = Puppeteer.createBrowserFetcher({ platform });
+//const revision = PUPPETEER_REVISIONS.chromium;
 
-browserFetcher
-  .download(revision, null)
-  .then(() => {
-    console.log("Chromium downloaded");
-    const parts = browserFetcher.revisionInfo(revision).executablePath.split(path.sep);
 
-      // based on where puppeteer puts the binaries see BrowserFetcher.revisionInfo()
-    while (!parts[parts.length - 1].startsWith('chrome-')) {
-      parts.pop()
-    }
+async function download() {
+    const buildId = await resolveBuildId(Browser.CHROME, platform, 'latest')
+    console.log(`Installing ${Browser.CHROME} into ${outputPath}`);
+    return install({
+        cacheDir: outputPath,
+        browser: Browser.CHROME,
+        platform,
+        buildId,
+        downloadProgressCallback: (
+            downloadedBytes,
+            totalBytes) => {
+            if (downloadedBytes % 1024 === 0) {
+            console.log(`Downloading ${downloadedBytes} out of ${totalBytes}`,);
+            }
+        }
+    })
+}
 
-    let execPath = parts.join(path.sep);
 
-    child_process.execSync(`cp -RP ${execPath} ${outputPath}`);
+download().then(() => {
+    console.log('Chrome downloaded into:', outputPath);
+})
 
-    console.log(`Chromium moved from ${execPath} to ${outputPath}/`);
-    process.exit(0);
-  })
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+
+
+
+
