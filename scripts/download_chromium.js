d1b980fdd8c4c530dbd666a4b09ea71d7db51db9
Clarity-89 <homes89@ukr.net>
2023-07-19 13:47:55 +0300

    Format

--- scripts/download_chromium.js
+++ scripts/download_chromium.js
@@ -1,42 +1,46 @@
 const path = require('path');
-const { BrowserPlatform, Browser, install, resolveBuildId } = require('@puppeteer/browsers')
+const {
+    BrowserPlatform,
+    Browser,
+    install,
+    resolveBuildId,
+} = require('@puppeteer/browsers');
 
 const archArg = process.argv[2];
 let [
     // Should be one of linux, mac, win32, win64 as per options in BrowserFetcher but we reuse the same arch string
     // as for grpc download (ie darwin-x64-unknown) so we need to transform it a bit
     platform,
-    arch
+    arch,
 ] = archArg.split('-');
 
 if (platform === 'win32' && arch === 'x64') {
-    platform = BrowserPlatform.WIN64
+    platform = BrowserPlatform.WIN64;
 }
-
 
 if (platform === 'darwin') {
-    platform = BrowserPlatform.MAC
+    platform = BrowserPlatform.MAC;
 }
 
-const outputPath = path.resolve(process.cwd(), "dist", process.argv[3] || `plugin-${archArg}`);
+const outputPath = path.resolve(
+    process.cwd(),
+    'dist',
+    process.argv[3] || `plugin-${archArg}`,
+);
+
+const browserVersion = Browser.CHROME;
 
 async function download() {
-    const browserVersion = Browser.CHROME;
-    const buildId = await resolveBuildId(browserVersion, platform, 'latest')
+    const buildId = await resolveBuildId(browserVersion, platform, 'latest');
     console.log(`Installing ${browserVersion} into ${outputPath}`);
     return install({
         cacheDir: outputPath,
         browser: browserVersion,
         platform,
         buildId,
-    })
+    });
 }
 
 download().then(() => {
-    console.log('Chrome downloaded into:', outputPath);
-})
-
-
-
-
-
+    console.log(`${browserVersion} downloaded into:`, outputPath);
+});
