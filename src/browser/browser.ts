8723f381ab7923f48fff8def14ea75762d6a8c88
Agnès Toulet <35176601+AgnesToulet@users.noreply.github.com>
2024-05-21 17:43:51 +0200

    use ID instead of class

--- src/browser/browser.ts
+++ src/browser/browser.ts
@@ -176,7 +176,7 @@ export class Browser {
   }
 
   async scrollToLoadAllPanels(page: puppeteer.Page, options: ImageRenderOptions): Promise<DashboardScrollingResult> {
-    const scrollDivSelector = '[class$="scrollbar-view"]';
+    const scrollDivSelector = '#page-scrollbar';
     const scrollDelay = options.scrollDelay ?? 500;
 
     await page.waitForSelector(scrollDivSelector);
