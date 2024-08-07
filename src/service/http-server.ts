1a0ee72b2f6373a77c3039774a090ccb895cd7df
Ezequiel Victorero <ezequiel.victorero@grafana.com>
2024-08-07 09:14:59 -0300

    remove unused packages and add logs

--- src/service/http-server.ts
+++ src/service/http-server.ts
@@ -20,7 +20,6 @@ import { Sanitizer } from '../sanitizer/Sanitizer';
 import { isSanitizeRequest } from '../sanitizer/types';
 import { asyncMiddleware, trustedUrlMiddleware, authTokenMiddleware } from './middlewares';
 import { SecureVersion } from 'tls';
-import {context, trace} from "@opentelemetry/api";
 
 const upload = multer({ storage: multer.memoryStorage() });
 
--- src/tracing.ts
+++ src/tracing.ts
@@ -1,17 +1,17 @@
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
-import { Resource } from '@opentelemetry/resources';
+import {NodeSDK} from '@opentelemetry/sdk-node';
+import {getNodeAutoInstrumentations} from '@opentelemetry/auto-instrumentations-node';
+import {OTLPTraceExporter} from '@opentelemetry/exporter-trace-otlp-http';
+import {SEMRESATTRS_SERVICE_NAME} from '@opentelemetry/semantic-conventions';
+import {Resource} from '@opentelemetry/resources';
+import {defaultServiceConfig} from "./service/config";
+import {ConsoleLogger} from "./logger";
 
-const process = require('process');
-// const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
-
-// For troubleshooting, set the log level to DiagLogLevel.DEBUG
-// diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 const traceExporter = new OTLPTraceExporter({
     url: 'http://localhost:4318/v1/traces', // Change to your Jaeger or OTLP endpoint
 });
+
+const logger = new ConsoleLogger(defaultServiceConfig.service.logging);
+logger.debug('Starting tracing');
 
 const sdk = new NodeSDK({
     resource: new Resource({
@@ -25,7 +25,7 @@ sdk.start();
 
 process.on('SIGTERM', () => {
     sdk.shutdown()
-        .then(() => console.log('Tracing terminated'))
-        .catch((error) => console.log('Error terminating tracing', error))
+        .then(() => logger.debug('Tracing terminated'))
+        .catch((error) => logger.error('Error terminating tracing', error))
         .finally(() => process.exit(0));
 });
