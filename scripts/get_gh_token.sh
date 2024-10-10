84379b1b29535b51c1f786544a8f5d19914beaf1
Agnès Toulet <35176601+AgnesToulet@users.noreply.github.com>
2024-10-10 17:08:28 +0200

    fix script

--- scripts/get_gh_token.sh
+++ scripts/get_gh_token.sh
@@ -12,10 +12,10 @@ chmod 600 private-key.pem
 # Generate the JWT
 NOW=$(date +%s)
 EXPIRATION=$(($NOW + 600))
-HEADER=$(printf \'{"alg":"RS256","typ":"JWT"}\' | openssl base64 -A | tr \'+/\' \'-_\' | tr -d \'=\')
-PAYLOAD=$(printf \'{"iat":%d,"exp":%d,"iss":"%s"}\' $NOW $EXPIRATION $GITHUB_APP_ID | openssl base64 -A | tr \'+/\' \'-_\' | tr -d \'=\')
+HEADER=$(printf '{"alg":"RS256","typ":"JWT"}' | openssl base64 -A | tr '+/' '-_' | tr -d '=')
+PAYLOAD=$(printf '{"iat":%d,"exp":%d,"iss":"%s"}' $NOW $EXPIRATION $GITHUB_APP_ID | openssl base64 -A | tr '+/' '-_' | tr -d '=')
 HEADER_PAYLOAD="$HEADER.$PAYLOAD"
-SIGNATURE=$(echo -n "$HEADER_PAYLOAD" | openssl dgst -sha256 -sign private-key.pem | openssl base64 -A | tr \'+/\' \'-_\' | tr -d \'=\')
+SIGNATURE=$(echo -n "$HEADER_PAYLOAD" | openssl dgst -sha256 -sign ./private-key.pem | openssl base64 -A | tr '+/' '-_' | tr -d '=')
 JWT="$HEADER_PAYLOAD.$SIGNATURE"
 
 # Request the installation access token
