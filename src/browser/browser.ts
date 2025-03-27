b9262fef2539208d20717d33aea3c7c1862b2748
siron <siron@leviathan.lan>
2025-03-27 11:20:51 +0100

    Fix to create a real PNG-8 for greyscaling

--- src/browser/browser.ts
+++ src/browser/browser.ts
@@ -6,6 +6,8 @@ import * as path from 'path';
 import * as fs from 'fs';
 import * as promClient from 'prom-client';
 import * as Jimp from 'jimp';
+import configure from "@jimp/custom";
+import png from "@jimp/png";
 import { Logger } from '../logger';
 import { RenderingConfig } from '../config/rendering';
 import { ImageRenderOptions, RenderOptions } from '../types';
@@ -417,11 +419,14 @@ export class Browser {
 
     if (options.greyScaleImage && !isPDF) {
       await this.performStep('greyScale', options.url, signal, async () => {
+        const jimp = configure({ types: [png] }, Jimp);
         const greyScaled = `${options.filePath}_${Date.now()}_greyscaled.png`;
         
-        const file = await Jimp.read(options.filePath);
+        const file = await jimp.read(options.filePath);
         await file
           .greyscale()
+          .deflateStrategy(0)
+          .colorType(0)
           .writeAsync(greyScaled);
 
         fs.renameSync(greyScaled, options.filePath);
