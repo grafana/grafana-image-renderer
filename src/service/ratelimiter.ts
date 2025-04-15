8b4cd235a2fe47f6fe6f0c2390ab36aa64d6361f
AgnesToulet <35176601+AgnesToulet@users.noreply.github.com>
2025-04-15 15:14:42 +0200

    add logs + devenv files

--- src/service/ratelimiter.ts
+++ src/service/ratelimiter.ts
@@ -2,8 +2,9 @@ import { RateLimiterRedis, RateLimiterMemory } from 'rate-limiter-flexible';
 import { Redis } from 'ioredis';
 
 import { RateLimiterConfig } from './config';
+import { Logger } from '../logger';
 
-export const setupRateLimiter = (config: RateLimiterConfig) => {
+export const setupRateLimiter = (config: RateLimiterConfig, log: Logger) => {
   let rateLimiter;
 
   if (config.redisHost && config.redisPort) {
@@ -18,12 +19,16 @@ export const setupRateLimiter = (config: RateLimiterConfig) => {
       points: config.requestsPerMinute, // Maximum number of requests
       duration: 60, // per 60 seconds
     });
+
+    log.info('Rate limiter enabled using Redis', 'requestsPerMinute', config.requestsPerMinute);
   } else {
     // Fallback to in-memory storage
     rateLimiter = new RateLimiterMemory({
       points: config.requestsPerMinute,
       duration: 60,
     });
+
+    log.info('Rate limiter enabled using in-memory storage', 'requestsPerMinute', config.requestsPerMinute);
   }
 
   return rateLimiter;
