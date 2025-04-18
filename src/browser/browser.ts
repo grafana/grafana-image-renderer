7e3c1bcf0edd7ecaa258f2654cdd70e8b97ee061
AgnesToulet <35176601+AgnesToulet@users.noreply.github.com>
2025-04-18 11:16:00 +0200

    fix unit

--- src/browser/browser.ts
+++ src/browser/browser.ts
@@ -391,7 +391,7 @@ export class Browser {
           },
           path: options.filePath,
           scale: 1 / scale,
-          timeout: options.timeout,
+          timeout: options.timeout * 1000,
         });
       }
 
