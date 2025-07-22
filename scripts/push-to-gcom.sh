d642be2c42a29708adf066ddeb424168ca272f0e
Mariell Hoversholm <mariell@mardroemmar.dev>
2025-07-22 09:31:03 +0200

    Chore: Delete Drone

--- scripts/push-to-gcom.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-JSON="$(cat ./scripts/tmp/plugin.json)"
-
-echo "$JSON"
-echo "Pushing..."
-
-curl -s -H "User-Agent: $GCOM_UAGENT" -H "Authorization: Bearer $GCOM_PUBLISH_TOKEN" "$GCOM_URL/plugins" -X POST -H "Content-Type: application/json" -d "$JSON"
