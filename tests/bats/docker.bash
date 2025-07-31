faf093c52580f42690976e00ff969c49d4a49df3
Mariell Hoversholm <mariell@mardroemmar.dev>
2025-07-31 11:08:19 +0200

    Tests: Fix bats tests (#696)

--- tests/bats/docker.bash
+++ tests/bats/docker.bash
@@ -29,19 +29,21 @@ _docker() {
     $SUDO "$DOCKER" "$@"
 }
 
-CONTAINER_NAME_PREFIX="${CONTAINER_NAME_PREFIX:-image-renderer-bats-}"
+CONTAINER_NAME_PREFIX="${CONTAINER_NAME_PREFIX:-image-renderer-bats}"
 
 _container_name() {
     local random_suffix
     random_suffix=$(tr -dc 'a-z0-9' < /dev/urandom | head -c 8)
-    echo "${CONTAINER_NAME_PREFIX}${random_suffix}"
+    echo "${CONTAINER_NAME_PREFIX}-${BATS_SUITE_TEST_NUMBER}-${random_suffix}"
 }
 
 _remove_docker() {
-    _docker ps -a --filter "name=${CONTAINER_NAME_PREFIX}" --format "{{.ID}}" | while read -r container_id; do
+    # The trailing dash is important to avoid removing unrelated containers.
+    # E.g. prefix-2 would match prefix-20-abc, but prefix-2- would not.
+    _docker ps -a --filter "name=${CONTAINER_NAME_PREFIX}-${BATS_SUITE_TEST_NUMBER}-" --format "{{.ID}}" | while read -r container_id; do
         if [ -n "$container_id" ]; then
             _docker rm -f "$container_id" || true # we don't really mind if we fail to clean up
-            echo "info: removed container $container_id"
+            echo "info: removed container $container_id" >&2
         fi
     done
 }
@@ -52,15 +54,19 @@ _wait_for_healthy() {
         return 1
     fi
     CONTAINER_NAME="$1"
-    TIMEOUT="${2:-10}" # default timeout is 10 seconds
-    START_TIME="$(date +%s)"
-    END_TIME=$((START_TIME + TIMEOUT))
-    while [ "$(date +%s)" -lt "$END_TIME" ]; do
-        if _docker inspect --format '{{.State.Health.Status}}' "$CONTAINER_NAME" | grep -q 'healthy'; then
+    TIMEOUT="${2:-30}"
+    # Stupid simple way to check timeouts. The docker commands are practically instant, so we just *5 (as we sleep 0.2s at a time) and iterate.
+    for _i in $(seq 1 $((TIMEOUT * 5))); do
+        if [ "$(_docker inspect --format '{{.State.Running}}' "$CONTAINER_NAME")" != "true" ]; then
+            echo "error: container $CONTAINER_NAME is not running" >&2
+            return 1
+        fi
+        if [ "$(_docker inspect --format '{{.State.Health.Status}}' "$CONTAINER_NAME")" = "healthy" ]; then
             return 0
         fi
         sleep 0.2
     done
     echo "error: container $CONTAINER_NAME did not become healthy within $TIMEOUT seconds" >&2
+    docker logs "$CONTAINER_NAME" >&2 || true # print the container log if possible.
     return 1
 }
