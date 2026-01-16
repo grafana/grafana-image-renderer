6eaf51ba6cffa86c97fb7a05be50395d809fd3fd
Matheus Macabu <macabu@users.noreply.github.com>
2026-01-16 14:13:06 +0100

    API: Add config flag for default encoding in rendering endpoint (#909)

--- cmd/server/cmd.go
+++ cmd/server/cmd.go
@@ -21,7 +21,7 @@ func NewCmd() *cli.Command {
 	return &cli.Command{
 		Name:   "server",
 		Usage:  "Run the server part of the service.",
-		Flags:  slices.Concat(config.ServerFlags(), config.TracingFlags(), config.BrowserFlags(), config.RateLimitFlags()),
+		Flags:  slices.Concat(config.ServerFlags(), config.TracingFlags(), config.BrowserFlags(), config.RateLimitFlags(), config.APIFlags()),
 		Action: run,
 	}
 }
@@ -31,6 +31,7 @@ type Cfg struct {
 	browser   config.BrowserConfig
 	tracing   config.TracingConfig
 	rateLimit config.RateLimitConfig
+	api       config.APIConfig
 }
 
 func ParseConfig(c *cli.Command) (*Cfg, error) {
@@ -50,11 +51,16 @@ func ParseConfig(c *cli.Command) (*Cfg, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse process tracker config: %w", err)
 	}
+	apiConfig, err := config.APIConfigFromCommand(c)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse api config: %w", err)
+	}
 	return &Cfg{
 		server:    serverConfig,
 		browser:   browserConfig,
 		tracing:   tracingConfig,
 		rateLimit: rateLimitConfig,
+		api:       apiConfig,
 	}, nil
 }
 
@@ -86,7 +92,7 @@ func run(ctx context.Context, c *cli.Command) error {
 	browser := service.NewBrowserService(cfg.browser, processStatService)
 	versions := service.NewVersionService()
 	metrics := metrics.NewRegistry()
-	handler, err := api.NewHandler(metrics, cfg.server, cfg.rateLimit, processStatService, browser, versions)
+	handler, err := api.NewHandler(metrics, cfg.server, cfg.api, cfg.rateLimit, processStatService, browser, versions)
 	if err != nil {
 		return fmt.Errorf("failed to create API handler: %w", err)
 	}
